4848eae1a20f469de56929fc32367a6096543e56
Jordan Gibbings <jordang@familyzone.com>
2023-07-18 18:22:48 +0800

    upgrade libcst + accompanying code updates, test with python3.11,  remove extraneous content from original repo

--- setup.py
+++ setup.py
@@ -6,21 +6,16 @@ with open("README.md", "r") as readme:
 
 setup(
     name="tornado-async-transformer",
-    version="0.2.0",
+    version="0.3.0",
     description="libcst transformer and codemod for updating tornado @gen.coroutine syntax to python3.5+ native async/await",
-    url="https://github.com/zhammer/tornado-async-transformer",
-    packages=find_packages(exclude=["tests", "demo_site"]),
-    package_data={"tornado_async_transformer": ["py.typed"]},
-    install_requires=["libcst == 0.2.4"],
-    author="Zach Hammer",
-    author_email="zachary_hammer@alumni.brown.edu",
+    url="https://github.com/jgibo/tornado-async-transformer",
+    packages=find_packages(),
+    install_requires=["libcst == 1.0.1"],
+    author="Zach Hammer, Jordan Gibbings",
     license="MIT License",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Libraries",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.11",
     ],
 )
